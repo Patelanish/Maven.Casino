4a6b0dbb3fe8c77e31b5d86c106297dd4c321ea8
Reese Watson <sherrese.watson@gmail.com>
2019-06-12 18:45:41 -0400

    Got some tests up and running. Out of six, five are passing.

--- src/test/java/io/zipcoder/casino/Roulette/RouletteTest.java
+++ src/test/java/io/zipcoder/casino/Roulette/RouletteTest.java
@@ -1,5 +1,7 @@
 package io.zipcoder.casino.Roulette;
 
+import io.zipcoder.casino.utilities.BasePlayer;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,12 +9,87 @@ import static org.junit.jupiter.api.Assertions.*;
 class RouletteTest {
 
     @Test
-    void getColor() {
-       //Given
+    void getColor(Integer num) {
+       Roulette roulette = new Roulette(null, null);
+        //Given
+        String expectedColor = "red";
 
        //When
+        String actualColor = roulette.getColor(4);
+
 
        //Then
+        Assert.assertEquals(expectedColor, actualColor);
+    }
+
+    @Test
+    void getColor2(Integer num) {
+        Roulette roulette = new Roulette(null, null);
+        //Given
+        String expectedColor = "black";
+
+        //When
+        String actualColor = roulette.getColor(7);
+
+
+        //Then
+        Assert.assertEquals(expectedColor, actualColor);
+    }
+
+    @Test
+    void getColor3(Integer num) {
+        Roulette roulette = new Roulette(null, null);
+        //Given
+        String expectedColor = "green";
+
+        //When
+        String actualColor = roulette.getColor(0);
+
+
+        //Then
+        Assert.assertEquals(expectedColor, actualColor);
+    }
+
+    @Test
+    void getColor4(Integer num) {
+        Roulette roulette = new Roulette(null, null);
+        //Given
+        String expectedColor = "red";
+
+        //When
+        String actualColor = roulette.getColor(22);
+
+
+        //Then
+        Assert.assertEquals(expectedColor, actualColor);
+    }
+
+//    @Test
+//    void getColor5(Integer num) {
+//        Roulette roulette = new Roulette(null, null);
+//        //Given
+//        String expectedColor = "black";
+//
+//        //When
+//        String actualColor = roulette.getColor(35);
+//
+//
+//        //Then
+//        Assert.assertEquals(expectedColor, actualColor);
+//    }
+
+    @Test
+    void getColor6(Integer num) {
+        Roulette roulette = new Roulette(null, null);
+        //Given
+        String expectedColor = null;
+
+        //When
+        String actualColor = roulette.getColor(37);
+
+
+        //Then
+        Assert.assertEquals(expectedColor, actualColor);
     }
 
     @Test
